0d8a0f2939dd27037b3825d33b8d62ac7978d1cf
nekonekomilk <milkcarramellz@yahoo.com>
2015-06-29 14:05:26 +0700

    Create rect.cpp

---
+++ rect.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "rect.h"
+
+float Rect::area(){	
+	return panjang*lebar;
+}
+
+float Rect::circum(){	
+	return (panjang+lebar)*2;
+}
+
+void Rect::detail(){
+	cout<< jenis_shape;
+	cout<< "Panjang		: "<< panjang<< endl;
+	cout<< "Lebar		: "<< lebar<< endl;
+	cout<< "Luas		: "<< area()<< endl;
+	cout<< "Keliling	: "<< circum()<< endl<< endl;
+}
+
+Rect::Rect(){
+	panjang=0;
+	lebar=0;
+	jenis_shape = "Persegi Panjang";
+}
+
+Rect::Rect(int panjang2, int lebar2){
+	panjang = panjang2;
+	lebar = lebar2;
+	jenis_shape = "Persegi Panjang";
+}
