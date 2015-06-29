f338adf2bb4ac40d6d22c93c042b240deda35a10
nekonekomilk <milkcarramellz@yahoo.com>
2015-06-29 14:04:21 +0700

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "square.h"
+
+float Square::area(){	
+	return sisi*sisi;
+}
+
+float Square::circum(){	
+	return sisi*4;
+}
+
+void Square::detail(){
+	cout<< jenis_shape;
+	cout<< "Sisi		: "<< sisi<< endl;
+	cout<< "Luas		: "<< area()<< endl;
+	cout<< "Keliling	: "<< circum()<< endl<< endl;
+}
+
+Square::Square(){
+	sisi=0;
+	jenis_shape = "Persegi";
+}
+
+Square::Square(int sisi2){
+	sisi = sisi2;
+	jenis_shape = "Persegi";
+}
